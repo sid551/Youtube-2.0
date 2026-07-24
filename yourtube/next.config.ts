f6785a267291e4af99e36d44d3b2c5e2ee3210d5
Siddharth Gupta <siddhu13072005@gmail.com>
2026-07-24 20:31:41 +0530

    Set Cross-Origin-Opener-Policy to unsafe-none for Google Auth popups

--- yourtube/next.config.ts
+++ yourtube/next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            value: "unsafe-none",
           },
         ],
       },
