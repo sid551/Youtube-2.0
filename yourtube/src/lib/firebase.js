67d24db1a49ae2a28f274b4b10bb931e3d613ea2
Siddharth Gupta <siddhu13072005@gmail.com>
2026-07-24 19:42:31 +0530

    Firebase auth configuration

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBr2K4HKC0AQxlDGbMST-sf1cKgAabVqN0",
-  authDomain: "yourtube-e9c7f.firebaseapp.com",
-  projectId: "yourtube-e9c7f",
-  storageBucket: "yourtube-e9c7f.firebasestorage.app",
-  messagingSenderId: "81976387475",
-  appId: "1:81976387475:web:122c4c5fd9e58cf2b72a66",
-  measurementId: "G-5X3883TQ7X"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBr2K4HKC0AQxlDGbMST-sf1cKgAabVqN0",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "yourtube-e9c7f.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "yourtube-e9c7f",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "yourtube-e9c7f.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "81976387475",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:81976387475:web:122c4c5fd9e58cf2b72a66",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-5X3883TQ7X",
 };
 
 // Initialize Firebase
